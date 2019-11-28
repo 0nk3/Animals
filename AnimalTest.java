fd04acc0e4d2d94074d185c4eb46a41383c8d4b7
Ndumiso Onke Fanti <0nk3.0nk3@gmail.com>
2019-11-28 09:36:00 +0200

    Added extra functionality to AnimalTest such that its not case sensitive

--- AnimalTest.java
+++ AnimalTest.java
@@ -11,11 +11,20 @@ class AnimalTest {
     void testDogSound(){
         assertEquals("Bark", dogObj.sounds(),"This test passes : Expected = Actual");
         assertEquals("barks", dogObj.sounds(), "This test case fails due to incorrect output : Bark v barks");
+
     }
     @Test
     void testDogEats(){
         assertEquals("Food", dogObj.eat(), "This test case passes : Expected = Actual");
         assertEquals("food", dogObj.eat(), "This test case fails due to case sensitivity of expected output : Food v food");
+
+        /* Extra functionality on test cases to ignore case sensitivity
+
+        *  compareToReturns 0 if two values that are being compared
+        *  are the same /equal. So for the test to pass the return value
+        *  must be >= ==> true
+        */
+        assert "Food".compareToIgnoreCase(dogObj.eat()) >= 0:"This test should pass because the test case is not case sensitive";
 
     }
     @Test
@@ -29,6 +38,9 @@ class AnimalTest {
     void testCatEats(){
         assertEquals("meat", catObj.eat(), "This test case fails : Food v meat ");
         assertEquals("Food", catObj.eat() , "This test case passes : Expected = Actual");
-        assertEquals("food", catObj.eat(), "This test case fails due to case sensitivity : Food v food");           // this test fails
+        assertEquals("food", catObj.eat(), "This test case fails due to case sensitivity : Food v food");
+
+        // Extra functionality on test cases to ignore case sensitivity
+        assert "food".compareToIgnoreCase(catObj.eat()) >= 0 : "This test case passes because 'Food' and and 'food'  are now viewed the same by compareToIgnoreCase";
     }
 }
